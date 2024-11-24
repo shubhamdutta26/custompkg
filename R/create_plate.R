051609f3c122f7d445b7036abe61e2e94a568cae
Shubham Dutta <shubhamdutta26@protonmail.com>
2024-11-24 17:46:08 -0500

    adding plate_dims

--- R/create_plate.R
+++ R/create_plate.R
@@ -51,6 +51,16 @@ create_plate <- function(plate_type = 96,
     stop("`file` must be a character string.", call. = FALSE)
   }
 
+  plate_dims <- list(
+    `6` = c(2, 3),
+    `12` = c(3, 4),
+    `24` = c(4, 6),
+    `48` = c(6, 8),
+    `96` = c(8, 12),
+    `384` = c(16, 24),
+    `1536` = c(32, 48)
+  )
+
   dims <- plate_dims[[as.character(plate_type)]]
   n_rows <- dims[1]
   n_cols <- dims[2]
